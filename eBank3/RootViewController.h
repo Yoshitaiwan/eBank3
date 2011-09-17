2adc7e61689088985e412770760675400336e98c
Yoshiyuki Matsuoka <yoshiyukimatsuoka@Yoshiyukis-MacBook-Air.local>
2011-09-17 14:06:06 +0800

    Initial Commit

---
+++ eBank3/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  eBank3
+//
+//  Created by Yoshiyuki Matsuoka on 17/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ eBank3/eBank3AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  eBank3AppDelegate.h
+//  eBank3
+//
+//  Created by Yoshiyuki Matsuoka on 17/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface eBank3AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
